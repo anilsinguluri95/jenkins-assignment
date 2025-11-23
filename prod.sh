3b67df510200986ff8419e9ca6f65c01a4be2c35
anilsinguluri95 <anilsinguluri95@gmail.com>
2025-11-23 12:46:43 +0530

    Create prod.sh

---
+++ prod.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "**** PROD Deployment Started ****"
+
+echo "Pulling latest files"
+ls -l
+
+echo "Starting Deployment..."
+# your deployment commands
+echo "Deployed Successfully on PROD server"
+
+echo "**** PROD Deployment Completed ****"
